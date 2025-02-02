3b24f29126eed6caa4e89947a147c3e898bc5e72
Leonille Matunan <leonillematunan@gmail.com>
2025-02-01 22:28:58 -0800

    Feature - Updating how Study Spots are made, will now send to the main table and deleting request_rable, easier to manage

--- src/services/Admin/Admin.js
+++ src/services/Admin/Admin.js
@@ -5,79 +5,16 @@ import { supabase } from '../supabase/supabase'
 
 export const fetchStudyRequest = async () => {
     // Fetch data from studylocationrequest
-    const { data: studyLocationRequests, error: studyLocationRequestError } = await supabase
-        .from('studylocationrequest')
-        .select(`
-            *,
-            States (
-                abr
-            )
-        `);
-
-    if (studyLocationRequestError) {
-        throw studyLocationRequestError;
-    }
-
-    // Fetch data from StudyLocation with tags
-    const { data: studyLocations, error: studyLocationError } = await supabase
+    const { data, error } = await supabase
         .from('StudyLocation')
-        .select(`
-            *,
-            States (
-            name,
-                abr
-            ),
-            LocationTagList (
-                TagTypes (
-                    id
-                )
-            )
-        `);
-
-    if (studyLocationError) {
-        throw studyLocationError;
+        .select('*, University(name), States(abr)')
+    if (error) {
+        throw error
     }
 
-    // Add missing fields to studyLocations to match the structure of studyLocationRequests
-    const formattedStudyLocations = studyLocations.map(location => ({
-        ...location,
-        status: 'Approved',
-        reviewed_at: null,
-        reviewed_by: null,
-        description: null,
-        locationtag: location.LocationTagList.map(tag => tag.TagTypes.id) // Extract tag id
-    }));
+    return data
 
-    // Combine the results
-    const combinedResults = [...studyLocationRequests, ...formattedStudyLocations];
-
-    // Extract all tag IDs from the combined results
-    const allTagIds = combinedResults.flatMap(request => request.locationtag).filter(tag => tag !== null);
-
-    // Fetch tags from corresponding tag IDs
-    const { data: tags, error: tagError } = await supabase
-        .from('TagTypes')
-        .select('id, name')
-        .in('id', allTagIds);
-
-    if (tagError) {
-        throw tagError;
-    }
-
-    // Map tag IDs to tag names
-    const tagMap = tags.reduce((acc, tag) => {
-        acc[tag.id] = tag.name;
-        return acc;
-    }, {});
-
-    // Map tag IDs to tag names in the combined results
-    combinedResults.forEach(request => {
-        request.locationtag = request.locationtag.map(tagId => tagMap[tagId]);
-    });
-
-
-    return combinedResults;
-};
+}
 
 
 export const fetchUniversityRequest = async () => {
